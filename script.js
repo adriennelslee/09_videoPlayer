4d63c1f59a98bb5579eac9607eeb4e486ba937f7
adriennelslee <75977500+adriennelslee@users.noreply.github.com>
2021-03-26 16:51:17 +0000

    Add files via upload
    with full screen and resize features added

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const progressBar = player.querySelector(".progress__filled");
 const toggle = player.querySelector(".toggle");
 const skipButtons = player.querySelectorAll("[data-skip]");
 const ranges = player.querySelectorAll(".player__slider");
-const screenSize = player.querySelector(".full-screen");
+const fullScreen = player.querySelector(".full-screen");
 
 
 /* FUNCTIONS */
@@ -19,6 +19,9 @@ function togglePlay(){
      video.pause();
  }
 };
+
+/* const method = video.paused? "play": "pause";
+video[method](); */
 
 function updateBtn(){
     const icon = this.paused? "►" : "❚ ❚";
@@ -41,15 +44,19 @@ function playTime(){
 function scrub(event){
     const scrubTime = (event.offsetX / progress.offsetWidth) * video.duration;
     video.currentTime = scrubTime;
-}
+};
 
-/* function expand(){
-    player.classList.add("player:fullscreen"); 
+ function expand(event){
     player.classList.remove("player");
-} */
+    player.classList.add("player:fullscreen"); 
+    player.classList.add("player__video");
+}; 
 
-/* const method = video.paused? "play": "pause";
-video[method](); */
+function shrink(event){
+    player.classList.remove("player:fullscreen");
+    player.classList.remove("player__video");
+    player.classList.add("player");
+}
 
 /*EVENT LISTENERS */
 
@@ -61,6 +68,9 @@ skipButtons.forEach(button => button.addEventListener("click", skip));
 ranges.forEach(slider=> slider.addEventListener("change", handleRange));
 ranges.forEach(slider=> slider.addEventListener("mousemove", handleRange));
 video.addEventListener("timeupdate", playTime);
+
+fullScreen.addEventListener("click", expand);
+fullScreen.addEventListener("dblclick", shrink);
 
 let mousedown = false; 
 progress.addEventListener("click", scrub);
